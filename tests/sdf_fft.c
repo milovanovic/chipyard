7e5acd1311f550e1c12f6ca1e6db4f8b511774d2
Nikola Petrovic <p.z.nikola@etf.rs>
2022-10-10 14:26:04 +0200

    Changed data from 32b to 64b

--- tests/sdf_fft.c
+++ tests/sdf_fft.c
@@ -5,14 +5,14 @@
 
 int main(void)
 {
-	volatile uint32_t r_size = 0;
-	uint32_t w_size = 7;
+	volatile uint64_t r_size = 0;
+	uint64_t w_size = 7;
 
-	reg_write32(SDF_FFT_SIZE, w_size);
-	r_size = reg_read32(SDF_FFT_SIZE);
+	reg_write64(SDF_FFT_SIZE, w_size);
+	r_size = reg_read64(SDF_FFT_SIZE);
 
 	if (r_size != w_size) {
-      printf("FAIL: Read value and writen value differed: %d %d", r_size, w_size);
+      printf("FAIL: Read value and writen value differed: %ld %ld", r_size, w_size);
       return -1;
     }
 
